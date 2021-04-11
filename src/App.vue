2d11475fcce0bd4a83dbaea4c04e07c0db5dd3af
陈泽华 <chenzehua@chenzehuadeiMac.local>
2021-04-11 23:59:00 +0800

    添加4.11

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,31 @@
 <template>
+  <div class="wrapper"></div>
   <div class="docker">
+    <div class="docker__item docker__item--active">
+      <span class="iconfont" style="display: inline-block">&#xe60b;</span>
+      <div class="docker__title">首页</div>
+    </div>
     <div class="docker__item">
       <span class="iconfont" style="display: inline-block">&#xe60b;</span>
-      首页
+      <div class="docker__title">购物车</div>
     </div>
-    <div class="docker__item">购物车</div>
-    <div class="docker__item">订单</div>
-    <div class="docker__item">我的</div>
+    <div class="docker__item">
+      <span class="iconfont" style="display: inline-block">&#xe60b;</span>
+      <div class="docker__title">订单</div>
+    </div>
+    <div class="docker__item">
+      <span class="iconfont" style="display: inline-block">&#xe60b;</span>
+      <div class="docker__title">我的</div>
+    </div>
   </div>
 </template>
 
 <style lang="scss">
+.wrapper {
+  position: absolute;
+  left: 0;
+  top: 0;
+}
 .docker {
   position: absolute;
   left: 0;
@@ -21,9 +36,22 @@
   display: flex;
   width: 100%;
   border-top: 1px solid #f1f1f1;
+  &__item {
+    flex: 1;
+    text-align: center;
+    .iconfont {
+      margin: 0.07rem 0 0.02rem 0;
+      font-size: 0.18rem;
+    }
+  }
+  &__title {
+    transform: scale(0.5, 0.5);
+    font-size: 0.2rem;
+    transform-origin: center top;
+  }
 }
-.docker__item {
-  flex: 1;
-  text-align: center;
+
+.docker__item--active {
+  color: #1fa4fc;
 }
 </style>
