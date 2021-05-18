aaba62424b9251922f6fd903c9e42025d7b32094
zehua <zehua@zehuadeiMac.local>
2021-05-18 19:57:46 +0800

    修改样式

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
+<!--
+ * @Author: zeHua
+ * @Date: 2021-05-18 17:40:45
+ * @LastEditors: zeHua
+ * @LastEditTime: 2021-05-18 19:57:36
+ * @FilePath: /imooc/vue3.0_jd/src/App.vue
+-->
 <template>
   <div>
-    <div class="wrapper"></div>
+    <div class="wrapper">
+      <div class="position">
+        <span class="iconfont position-icon"></span>
+        北京理工大学北校区103室
+      </div>
+    </div>
     <div class="docker">
       <div class="docker__item docker__item--active">
         <span class="iconfont icon-home" style="display: inline-block"></span>
@@ -30,6 +42,16 @@
   position: absolute;
   left: 0;
   top: 0;
+  right: 0;
+  bottom: 0.5rem;
+  padding: 0 0.18rem;
+}
+.position {
+  padding: 0.16rem 0;
+  line-height: 0.22rem;
+  font-size: 0.16rem;
+  &__icon {
+  }
 }
 .docker {
   position: absolute;
